b58b658f0220eb4c51e584916ff0ccabb6d3b152
riibka <riibkins@yandex.ru>
2024-11-18 13:26:33 +0300

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+lua54 'yes'
+fx_version 'cerulean'
+game 'rdr3'
+
+description 'nexus-camera'
+
+author 'justclownfish'
+version '0.0.1'
+
+dependencies {
+    'oxmysql',
+    'ox_lib',
+    'rsg-core',
+}
+
+shared_script {
+    'config.lua',
+    '@ox_lib/init.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/*.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
